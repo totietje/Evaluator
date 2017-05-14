bfec8ac0f6ca465e000e05f87a1a0ae06b9bbd1f
totietje <salv.elia@hotmail.com>
2017-05-14 14:26:53 +0100

    Documented ComplexEvaluator

--- src/main/scala/net/totietje/complex/ComplexEvaluator.scala
+++ src/main/scala/net/totietje/complex/ComplexEvaluator.scala
@@ -4,6 +4,53 @@ import ComplexFunctionToken._
 import net.totietje.evaluator.Token._
 import net.totietje.evaluator.{AbstractEvaluator, EvaluationException, Token}
 
+/** An [[net.totietje.evaluator.Evaluator Evaluator]] which parses an expression as a
+  * [[net.totietje.complex.ComplexFunction ComplexFunction]].
+  *
+  * This allows the basic operators `+`, `-`, `*`, `/` and `^` and the use of brackets.
+  *
+  * The following strings are treated as constants:
+  *   - i
+  *   - pi or π
+  *   - tau or τ
+  *   - e
+  *
+  * Furthermore, the following functions are recognised:
+  *   - im
+  *   - re
+  *   - arg
+  *   - abs
+  *   - sqrt or √
+  *   - log
+  *   - sin
+  *   - asin
+  *   - cos
+  *   - acos
+  *   - tan
+  *   - atan
+  *   - sinh
+  *   - asinh
+  *   - cosh
+  *   - acosh
+  *   - tanh
+  *   - atanh
+  *
+  * There is currently no support for implicit multiplication without the `*` symbol, such as `2i`.
+  *
+  * A string can be parsed like this:
+  * {{{
+  *   val complexFunction = ComplexEvaluator.evaluate("2 + i * sin(x)")
+  * }}}
+  *
+  * That returned `ComplexFunction` can then be evaluated for a given input for `x` with:
+  * {{{
+  *   val complexNumber = complexFunction(Map("x" -> xValue))
+  * }}}
+  *
+  * For example, replacing `xValue` with `Complex.Pi` would produce a complex number roughly equal to 2.
+  * It is recommended to then round the result by calling `.round` to round the number to an appropriate degree
+  * of accuracy.
+  */
 object ComplexEvaluator extends AbstractEvaluator[ComplexFunction] {
   override protected def isNumeric(char: Char): Boolean = char.isDigit || char == '.'
   
