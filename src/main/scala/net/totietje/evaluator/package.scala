3312d643e8440237359b23f7e14c2b8fb906b327
totietje <salv.elia@hotmail.com>
2017-05-16 17:21:03 +0100

    Documented evaluator package

---
+++ src/main/scala/net/totietje/evaluator/package.scala
@@ -0,0 +1,13 @@
+package net.totietje
+
+/** The `evaluator` package is designed to simplify the process of parsing a string.
+  *
+  * The work of parsing a string is reduced to just tokenizing an expression, that is, breaking it down into its
+  * principal parts. The `Evaluator` class will handle the rest.
+  *
+  * The `AbstractEvaluator` makes this even easier, though it is only applicable in some situations.
+  *
+  * @see [[net.totietje.evaluator.Evaluator Evaluator]]
+  * @see [[net.totietje.evaluator.AbstractEvaluator AbstractEvaluator]]
+  */
+package object evaluator
